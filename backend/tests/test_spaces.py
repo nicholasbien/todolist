638d5c7dd258578c6ee4ec3f64f7785491502fc4
Nicholas Bien <nicholasbien@users.noreply.github.com>
2025-06-15 17:16:27 -0400

    Fix space creation and add invitations

--- backend/tests/test_spaces.py
+++ backend/tests/test_spaces.py
@@ -15,18 +15,21 @@ async def get_token(client, email):
 @pytest.mark.asyncio
 async def test_create_space(client, test_email, test_email2):
     token1 = await get_token(client, test_email)
-    token2 = await get_token(client, test_email2)
-
     headers1 = {"Authorization": f"Bearer {token1}"}
-    headers2 = {"Authorization": f"Bearer {token2}"}
 
-    create_resp = await client.post(
-        "/spaces",
-        json={"name": "Collab", "member_emails": [test_email2]},
-        headers=headers1,
-    )
+    create_resp = await client.post("/spaces", json={"name": "Collab"}, headers=headers1)
     assert create_resp.status_code == 200
     space_id = create_resp.json()["_id"]
+
+    invite_resp = await client.post(
+        f"/spaces/{space_id}/invite",
+        json={"emails": [test_email2]},
+        headers=headers1,
+    )
+    assert invite_resp.status_code == 200
+
+    token2 = await get_token(client, test_email2)
+    headers2 = {"Authorization": f"Bearer {token2}"}
 
     list_resp = await client.get("/spaces", headers=headers2)
     assert list_resp.status_code == 200
