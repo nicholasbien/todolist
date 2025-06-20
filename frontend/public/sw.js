9edf3634123aac7730673bb933deb2656b4583e3
Nicholas Bien <nicholasbien@gmail.com>
2025-06-20 03:59:23 -0400

    bump version

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -3,7 +3,7 @@ const API_CACHE = 'todo-api-v38';
 
 const GLOBAL_DB_NAME = 'TodoGlobalDB';
 const USER_DB_PREFIX = 'TodoUserDB_';
-const DB_VERSION = 4;
+const DB_VERSION = 6;
 const TODOS = 'todos';
 const CATEGORIES = 'categories';
 const SPACES = 'spaces';
