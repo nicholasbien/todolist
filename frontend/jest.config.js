04d499579e915e10c3244d1d6c70ec2c477d2484
Nicholas Bien <nicholasbien@gmail.com>
2025-06-20 00:35:05 -0400

    sw fixes for spaces

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -5,6 +5,16 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  testMatch: [
+    '<rootDir>/tests/**/*.test.{js,jsx,ts,tsx}',
+    '<rootDir>/**/__tests__/**/*.{js,jsx,ts,tsx}'
+  ],
+  collectCoverageFrom: [
+    'public/sw.js',
+    'components/**/*.{js,jsx,ts,tsx}',
+    '!components/**/*.stories.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+  ],
 };
 
 module.exports = createJestConfig(customJestConfig);
