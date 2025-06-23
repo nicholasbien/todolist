22d01399446ff13f8788f6d9094b09eb258bc802
Nicholas Bien <nicholasbien@gmail.com>
2025-06-22 22:27:08 -0400

    Revert "force sw update"
    This reverts commit 013ea2fcbdef41315b26717ff7aedf1f4b5b5e8c.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -8,25 +8,9 @@ function MyApp({ Component, pageProps }: AppProps) {
     // Register service worker in all environments
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
-        .register(`/sw.js?v=${Date.now()}`, { updateViaCache: 'none' })
-        .then((registration) => {
-          // Force update check on load
-          registration.update();
-
-          // Listen for updates
-          registration.addEventListener('updatefound', () => {
-            const newWorker = registration.installing;
-            if (newWorker) {
-              newWorker.addEventListener('statechange', () => {
-                if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
-                  // New service worker is available, tell it to skip waiting
-                  newWorker.postMessage({ type: 'SKIP_WAITING' });
-                  console.log('New service worker available. Reloading...');
-                  window.location.reload();
-                }
-              });
-            }
-          });
+        .register('/sw.js')
+        .then(() => {
+          // Service Worker registered successfully
         })
         .catch((registrationError) => {
           console.log('Service Worker registration failed: ', registrationError);
